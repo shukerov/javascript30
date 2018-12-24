b84031359a21ed600d708ec5c87f89d8b93594ac
Stoyan Shukerov <stoichik@gmail.com>
2018-12-24 16:37:18 +0200

    drumkit almost complete

--- 01-drumkit/script.js
+++ 01-drumkit/script.js
@@ -1,9 +1,21 @@
 window.addEventListener('keydown', function(e) {
    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-   
+   const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+
    // if key pressed is not an audio element don't run
    if (!audio) return;
-
+   
+   key.classList.add('playing');
    audio.currentTime = 0;
    audio.play(); 
+
 });
+
+function removeTransition(e) {
+   if (e.propertyName !== 'transform') return; // skip if not a transform
+
+   this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
