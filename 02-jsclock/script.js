418308541d6e24a826c2d9b95e77f2a5578dec90
Stoyan Shukerov <stoichik@gmail.com>
2018-12-24 18:19:27 +0200

    fixing arrows jumping around when they should not

--- 02-jsclock/script.js
+++ 02-jsclock/script.js
@@ -1,20 +1,33 @@
+hands = document.querySelectorAll(".hand");
 secondHand = document.querySelector(".second-hand");
 minuteHand = document.querySelector(".min-hand");
 hourHand = document.querySelector(".hour-hand");
+removed_transition = false;
 
 function setDate() {
    const now = new Date();
+
    const seconds = now.getSeconds();
    const secondsDeg = ((seconds / 60) * 360) + 90;
-
-   secondHand.style.transform = `rotate(${secondsDeg}deg)`;
-
+   
    const mins = now.getMinutes();
    const minsDeg = ((mins / 60) * 360) + 90;
-   minuteHand.style.transform = `rotate(${minsDeg}deg)`;
 
    const hours = now.getHours();
    const hoursDeg = ((hours / 12) * 360) + 90;
+   
+   if (removed_transition) {
+      hands.forEach(hand => hand.classList.remove("hand_trans"));
+      removed_transition = false;
+   }
+
+   if (seconds == 0 || mins == 0 || hours == 12) {
+      hands.forEach(hand => hand.classList.add("hand_trans")); 
+      removed_transition = true;
+   }
+
+   secondHand.style.transform = `rotate(${secondsDeg}deg)`;
+   minuteHand.style.transform = `rotate(${minsDeg}deg)`;
    hourHand.style.transform = `rotate(${hoursDeg}deg)`;
 
    // for debugging purposes
