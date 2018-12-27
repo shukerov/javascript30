dffef23eb5e147bf0995ca565b81ac3b9b0591cd
Stoyan Shukerov <stoichik@gmail.com>
2018-12-27 14:42:11 +0200

    started project 6 search bar

---
+++ 06-ajax/script.js
@@ -0,0 +1,27 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+
+fetch(endpoint)
+   .then(blob => blob.json())
+   .then(data => cities.push(...data));
+
+function findMatches(wordToMatch, cities) {
+   return cities.filter(place => {
+      const regex = RegExp(wordToMatch, 'gi');
+      return place.city.match(regex) || place.state.match(regex);
+   });
+}
+
+function displayMatches() {
+   const matchArray = findMatches(this.value, cities);
+   console.log(matchArray);
+}
+
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
+STOPPED AT 13:59
