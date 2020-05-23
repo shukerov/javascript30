fc1a01377433889c66c304632e9454db829e547b
Stoyan Shukerov <stoichik@gmail.com>
2020-05-22 17:06:27 -0700

    exercise 10 finished

--- 10-checkbox/script.js
+++ 10-checkbox/script.js
@@ -1,1 +1,28 @@
-hi
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+// add evenet listener
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
+
+let lastChecked;
+
+function handleCheck(e) {
+   let inBetween = false;
+   if(e.shiftKey && this.checked) {
+      //blalblab
+      // loop over every single checkbox
+      checkboxes.forEach(c => {
+         console.log(c);
+         if(c === this || c === lastChecked) {
+            inBetween = !inBetween;
+            console.info("start-end");
+         }
+
+         if (inBetween) {
+            c.checked = true;
+         }
+      });
+      
+   }
+
+   lastChecked = this;
+}
